a186be8823e525b74197af0cb732d8f0033b8128
JohLpe <johanna.laporte@ymail.com>
2020-10-02 17:00:35 +0200

    addition restriction liste item tiles

--- items.py
+++ items.py
@@ -9,6 +9,7 @@ class Items:
         self.sprSize = 40
         self.image = image
         maze = open('maze.txt', 'r')
+        removalList = []
         accessibleTiles = []
         for y, line in enumerate(maze):
             y = y * self.sprSize
@@ -18,5 +19,17 @@ class Items:
                 elif letter == 'F':
                     x = x * self.sprSize
                     accessibleTiles.append((x, y))
+                elif letter == 'G':
+                    x = x * self.sprSize
+                    minX = x - self.sprSize
+                    maxX = x + (self.sprSize * 2)
+                    minY = y - self.sprSize
+                    maxY = y + (self.sprSize * 2)
+                    for y2 in range(minY, maxY, self.sprSize):
+                        for x2 in range(minX, maxX, self.sprSize):
+                            removalList.append((x2, y2))
+        for coordinates in removalList:
+            if coordinates in accessibleTiles:
+                accessibleTiles.remove(coordinates)
         i = random.randrange(0, len(accessibleTiles))
         self.plc = accessibleTiles[i]
