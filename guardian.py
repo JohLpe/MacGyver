78afbeb6eb40e084ac7b133c04a90c8aff759c90
JohLpe <johanna.laporte@ymail.com>
2020-10-16 18:36:00 +0200

    suppression variable inutile guardian

--- guardian.py
+++ guardian.py
@@ -7,7 +7,6 @@ class Guardian():
     def __init__(self):
 
         self.image = pg.image.load('ressource/guardian.png')
-        self.playArea = letter_to_sprite(['F', 'A', 'U', 'D', 'G'])
         self.plc = letter_to_sprite('G')
         self.plcArea = guardian_nearby_tiles()
 
